3be6f54995b3948fe416f9287e51f6673f0ff277
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2020-12-03 22:08:52 +0100

    Day1

---
+++ day1/day1.hs
@@ -0,0 +1,15 @@
+import Data.List
+
+nothingToZero::Maybe Int -> Int
+nothingToZero Nothing = 0
+nothingToZero (Just n) = 1
+
+main::IO ()
+main = do
+  inputs <- lines <$> readFile "./input"
+  let lists = let toI s = read s :: Int in map toI inputs
+  let pairs = map (2020-) lists
+  let pairIndices =  let fiLists n = elemIndex n lists in map fiLists pairs
+  let pairIndicesNum = map nothingToZero pairIndices
+  let result = transpose [lists, pairs, pairIndicesNum]
+  print $ filter (>0) (map product result)
