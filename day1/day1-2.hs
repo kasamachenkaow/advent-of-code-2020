3f1dcedde8692a946e622a8009ac308dcdbc17d8
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2020-12-04 00:50:07 +0100

    Day2

---
+++ day1/day1-2.hs
@@ -0,0 +1,50 @@
+import Data.List
+import Debug.Trace
+
+
+emptyToZeros::[[Int]] -> [[Int]]
+emptyToZeros [] = [[0]]
+emptyToZeros lists = lists
+
+nothingToZero::Maybe Int -> Int
+nothingToZero Nothing = 0
+nothingToZero (Just n) = 1
+
+notZeroProduct::[Int] -> Bool
+notZeroProduct items = nonZero where
+  nonZero = (product items) > 0
+
+notEmptyArray::[Int] -> Bool
+notEmptyArray items = not (null items)
+
+-- findProductOfPairOf::([Int], Int, Int) -> [Int]
+-- findProductOfPairOf (lists, sumOf, 2) = result where
+--   result = head (filter nonZeroProduct validPairs)
+--   sss = print validPairs
+--   validPairs = transpose [lists, pairs, pairIndexExists]
+--   pairIndexExists = map nothingToZero pairIndices
+--   pairIndices =  let fiLists n = elemIndex n lists in map fiLists pairs
+--   pairs = map (sumOf-) lists
+-- findProductOfPairOf (lists, sumOf, itemCount) = let findOthers n = n : findProductOfPairOf (lists, sumOf-n, itemCount-1) in head (filter nonEmptyArray (map findOthers lists))
+
+findProductOfPairOf::([Int], Int, Int) -> [Int]
+findProductOfPairOf (lists, sumOf, 2) = do
+  let pairs = map (sumOf-) lists
+  let pairIndices =  let fiLists n = elemIndex n lists in map fiLists pairs
+  let pairIndexExists = map nothingToZero pairIndices
+  let validPairs = transpose [lists, pairs, pairIndexExists]
+  result <- head (emptyToZeros (filter notZeroProduct validPairs))
+  return result
+findProductOfPairOf (lists, sumOf, itemCount) = do
+  result <- let findOthers n = n : (findProductOfPairOf (filter (\p -> p /= n) lists, sumOf-n, itemCount-1)) in head (emptyToZeros (filter notZeroProduct (map findOthers lists)))
+  return result
+
+main::IO ()
+main = do
+  inputs <- lines <$> readFile "./input"
+  let lists = let toI s = read s :: Int in map toI inputs
+  let pairs = (findProductOfPairOf (lists, 2020, 3))
+  traceShowM "pairs"
+  traceShowM pairs
+  let result = product pairs
+  print result
