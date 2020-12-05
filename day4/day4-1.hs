29d781daed6e71d8139f21283018adeb81b75356
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2020-12-06 00:21:23 +0100

    Day4.1

---
+++ day4/day4-1.hs
@@ -0,0 +1,22 @@
+import Data.List
+import Data.List.Split
+
+getKeys::String -> String
+getKeys keyValue = take 3 keyValue
+
+validate::[String] -> Bool
+validate ["byr", "cid", "ecl", "eyr", "hcl", "hgt", "iyr", "pid"] = True
+validate ["byr", "ecl", "eyr", "hcl", "hgt", "iyr", "pid"] = True
+validate xs = False
+
+main :: IO ()
+main = do
+  rawInputs <- lines <$> readFile "./input"
+  let splitArrayInput = splitOn [""] rawInputs
+  let mergeSplitArray = map unwords splitArrayInput
+  let splitKeyValueArray = map (splitOn " ") mergeSplitArray
+  let passportKeysArray = map (map getKeys) splitKeyValueArray
+  let sortedPassportKeysArray = map sort passportKeysArray
+  let validatedResult = map validate sortedPassportKeysArray
+  let validCount = length $ filter (==True) validatedResult
+  print $ validCount
