8156fdc6aeb9b35694e1ee8e8d134560c53ffb61
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2020-12-05 23:31:38 +0100

    Day3.2

---
+++ day3/day3-2.hs
@@ -0,0 +1,24 @@
+import Data.List
+import Data.List.Split
+
+removeEvenItem::[a] -> [a]
+removeEvenItem (x:y:xs) = x : removeEvenItem xs
+removeEvenItem xs = []
+
+boolToInt::Bool -> Int
+boolToInt True = 1
+boolToInt False = 0
+
+countTrees::Int -> [String] -> Int -> Int
+countTrees pos (m:ms) stepRight = boolToInt (m !! pos == '#') + (countTrees ((pos + stepRight) `mod` (length m)) ms stepRight)
+countTrees pos ms stepRight = 0
+
+main :: IO ()
+main = do
+  rawInputs <- lines <$> readFile "./input"
+  let right1down1 = countTrees 0 rawInputs 1
+  let right3down1 = countTrees 0 rawInputs 3
+  let right5down1 = countTrees 0 rawInputs 5
+  let right7down1 = countTrees 0 rawInputs 7
+  let right1down2 = countTrees 0 (removeEvenItem rawInputs) 1
+  print $ right1down1 * right3down1 * right5down1 * right7down1 * right1down2
