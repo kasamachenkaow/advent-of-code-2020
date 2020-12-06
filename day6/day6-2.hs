fe45cb6b85fa53d02c1bdae6e0e85cd11ad1c3b5
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2020-12-06 17:01:48 +0100

    Day6.2

---
+++ day6/day6-2.hs
@@ -0,0 +1,15 @@
+import Data.List
+import Data.List.Split
+
+intersectCharList :: [[Char]] -> [Char]
+intersectCharList (x:xs) = intersect x (intersectCharList xs)
+intersectCharList xs = ['a'..'z']
+
+main :: IO ()
+main = do
+  rawInputs <- lines <$> readFile "./input"
+  let groupArraysByEmptyLine = splitOn [""] rawInputs
+  let sharedQuestionsGroupss = map intersectCharList groupArraysByEmptyLine
+  let sharedQuestionsGroupCounts = map length sharedQuestionsGroupss
+  let sumCount = sum sharedQuestionsGroupCounts
+  print sumCount
