0490ad4ea95d922177c644f48d3c44c7fe9616ab
Kasama Chenkaow <kasama.chenkaow@klarna.com>
2020-12-12 23:24:15 +0100

    Day10-2d

--- Common.hs
+++ Common.hs
@@ -3,3 +3,7 @@ module Common where
 toInt :: String -> Int
 toInt s = read s :: Int
 
+readInputByLine :: String -> IO [String]
+readInputByLine path = do
+    rawInputs <- lines <$> readFile path
+    return rawInputs
---
+++ day10/day10-2d.hs
@@ -0,0 +1,40 @@
+-- Far better performance, time complexity: O(n), Space O(n)
+
+import Data.List
+import Data.List.Split
+import Common
+
+atLeast3missingCount :: Int -> Int
+atLeast3missingCount 0 = 0
+atLeast3missingCount 1 = 0
+atLeast3missingCount 2 = 0
+atLeast3missingCount 3 = 1
+atLeast3missingCount n = 2^(n-3)  + atLeast3missingCount (n-1)
+
+toMultiplier :: Int -> Int
+toMultiplier n = 2^body - atLeast3missingCount body
+  where
+    body = seqNum-2
+    seqNum = n+1
+
+main :: IO ()
+main = do
+  rawInputs <- readInputByLine "./input"
+  let numbers = map toInt rawInputs
+  let outletJoltage = 0
+  let adapterJoltages = sort numbers
+  let deviceJoltage = last adapterJoltages + 3
+  let allJoltages = [outletJoltage] ++ adapterJoltages ++ [deviceJoltage]
+  let differences = [ allJoltages !! i - allJoltages !! (i-1) | i <- [1..length allJoltages-1]]
+  let groupOf1s = filter (not . null) (splitOn [3] differences)
+  let groupOf1sCount = [ length ones | ones <- groupOf1s]
+  let mulipliers = map toMultiplier groupOf1sCount
+  print $ allJoltages
+  print $ differences
+  print $ groupOf1s
+  print $ groupOf1sCount
+  print $ mulipliers
+  print $ product mulipliers
+
+-- Input2 : 8
+-- Input3 : 19208
